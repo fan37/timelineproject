9c37740b1e86082132ae2ea30f14197427fb1f5d
fan37 <fan37@naver.com>
2017-03-01 00:00:32 +0900

    aaaa

--- manage.py
+++ manage.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timelineproject.settings")
 
     from django.core.management import execute_from_command_line
-
+	aaaa
     execute_from_command_line(sys.argv)
